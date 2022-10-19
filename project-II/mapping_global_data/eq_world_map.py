b637c4a6ba0bfcade8e9de7544802add2cf22cbe
Ziya Alim <ziyabay135@gmail.com>
2022-10-19 11:47:38 +0300

    used plotly to make a plot of earthquakes happening around the world

---
+++ project-II/mapping_global_data/eq_world_map.py
@@ -0,0 +1,40 @@
+import json
+from plotly.graph_objects import Scattergeo, Layout
+from plotly import offline
+
+file_name = 'data/eq_data_30_day_m1.json'
+with open(file_name) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(title)
+
+# Map the earthquakes.
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'YlGnBu',
+        'reversescale': False,
+        'colorbar': {'title': 'Magnitude'},
+    },
+}]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig)           # You can add the filename you want to save it as after the comma
+
